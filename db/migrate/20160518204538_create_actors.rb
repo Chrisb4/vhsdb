a9d0dcf9d8314b1731872fbaccbaafcec4a334fb
Christopher Brown <nudgechris@gmail.com>
2016-05-18 16:00:40 -0500

    fixed actors

--- db/migrate/20160518204538_create_actors.rb
+++ db/migrate/20160518204538_create_actors.rb
@@ -2,7 +2,7 @@ class CreateActors < ActiveRecord::Migration
   def change
     create_table :actors do |t|
       t.string :name
-      t.string :birth_year
+      t.date :birth_year
       t.string :thumbnail
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518201934) do
+ActiveRecord::Schema.define(version: 20160518204538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "actors", force: :cascade do |t|
     t.string   "name"
-    t.string   "birth_year"
+    t.date     "birth_year"
     t.string   "thumbnail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
